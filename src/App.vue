a4c668257fd683ccf567680bba88a7feb58de388
Ruolodi <killer777hunter@gmail.com>
2022-02-26 17:47:16 +0300

    fixWorked

--- src/App.vue
+++ src/App.vue
@@ -6,11 +6,12 @@
         v-for="(items, id) in cards"
         :key="items.id"
         @click="
-          isClicked ? `${clickCards1(items, id)}` : `${clickCards(items, id)}`
+          isClicked ? `${clickCards1(items, id)}` : `${clickCards(items, id)}`,
+            (isClicked = !isClicked)
         "
         :disabled="cardsCheck[id]"
       >
-        <p>{{ items }}</p>
+        <p v-if="cardsCheck[id]">{{ items }}</p>
       </button>
     </div>
   </div>
@@ -20,26 +21,11 @@
 export default {
   data() {
     return {
-      cards: ["4", "2", "3", "4", "1", "2", "3", "1", "1", "2", "3", "1"],
-      cardsCheck: [
-        false,
-        false,
-        false,
-        false,
-        false,
-        false,
-        false,
-        false,
-        false,
-        false,
-        false,
-        false,
-      ],
+      cards: ["4", "2", "3", "4", "1", "2", "3", "1", "5", "6", "5", "6"],
+      cardsCheck: [false, false, false, false, false, false, false, false],
       isClicked: false,
       isNumber: "",
       isNumber1: "",
-      idSaver: "",
-      idSaver1: "",
     }
   },
   methods: {
@@ -47,7 +33,7 @@ export default {
       this.isNumber = items
       this.cardsCheck[id] = true
       console.log(this.cardsCheck)
-      this.isClicked = true
+
       this.idSaver = id
       console.log("id", id)
     },
@@ -55,18 +41,37 @@ export default {
       this.isNumber1 = items
       this.cardsCheck[id] = true
       let isNumberSaver = this.isNumber
-      this.idSaver1 = id
       if (this.isNumber === this.isNumber1) {
-        console.log("id1", id)
-        this.cardsCheck.splice(id, 1)
-        this.cardsCheck.splice(id, 1)
-        this.cards = this.cards.filter((f) => {
-          return f !== isNumberSaver
-        })
-        this.isClicked = false
+        setTimeout(() => {
+          console.log("id1", id)
+          this.cards = this.cards.filter((f) => {
+            return f !== isNumberSaver
+          })
+          this.cardsCheck = this.cardsCheck.filter((f) => {
+            return f !== true
+          })
+        }, 800)
+      } else {
+        setTimeout(() => {
+          this.cardsCheck = [
+            false,
+            false,
+            false,
+            false,
+            false,
+            false,
+            false,
+            false,
+          ]
+        }, 800)
       }
       /* this.isClicked = false */
-      console.log(id)
+      console.log(
+        "this.isNumber",
+        this.isNumber,
+        "this.isNumber1",
+        this.isNumber1
+      )
     },
   },
 }
