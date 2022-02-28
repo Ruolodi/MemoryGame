8a30764f4bacada9a5c3823b384b3d1aa433c89f
Pavel <killer777hunter@gmail.com>
2022-02-28 14:11:24 +0300

    generator

--- src/App.vue
+++ src/App.vue
@@ -14,6 +14,7 @@
         <p v-if="cardsCheck[id]">{{ items }}</p>
       </button>
     </div>
+    <button class="btn" @click="generator">Генерация</button>
   </div>
 </template>
 
@@ -39,9 +40,26 @@ export default {
       isClicked: false,
       isNumber: "",
       isNumber1: "",
+      generatorCounter:6,
     }
   },
   methods: {
+    generator(){
+      let arrGenerated = []
+      for(let i = 0; i<this.generatorCounter; i++){
+       let randomNumber = Math.floor(Math.random() * (10 - 1 + 1) + 1)
+       console.log('i',i)
+       if(arrGenerated.includes(randomNumber)){
+         console.log('povtor','i',i)
+         i--
+       }
+       else{
+         arrGenerated.push(randomNumber,randomNumber)
+         arrGenerated.join('')
+         console.log(arrGenerated)
+       }
+      }
+    },
     clickCards(items, id) {
       this.isNumber = items
       this.cardsCheck[id] = true
@@ -51,12 +69,11 @@ export default {
     clickCards1(items, id) {
       this.isNumber1 = items
       this.cardsCheck[id] = true
-      let isNumberSaver = this.isNumber
       if (this.isNumber === this.isNumber1) {
         setTimeout(() => {
-          console.log("id1", id)
+          console.log("isNumber", this.isNumber)
           this.cards = this.cards.filter((f) => {
-            return f !== isNumberSaver
+            return f !== this.isNumber
           })
           this.cardsCheck = this.cardsCheck.filter((f) => {
             return f !== true
